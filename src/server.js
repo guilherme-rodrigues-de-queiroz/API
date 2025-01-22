56400b33a7f8043def8f88a2b5b10bce6f222068
Guilherme Rodrigues <147875655+guilherme-rodrigues-de-queiroz@users.noreply.github.com>
2025-01-22 16:22:19 -0300

    server.js

--- src/server.js
+++ src/server.js
@@ -7,17 +7,10 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: 'https://cadastro-usuarios-kappa.vercel.app',
+    origin: 'https://cadastro-usuarios-kappa.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
 }))
-
-app.options('*', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://cadastro-usuarios-kappa.vercel.app')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    res.status(200).end()
-})
 
 const port = process.env.PORT ?? 3000
 
